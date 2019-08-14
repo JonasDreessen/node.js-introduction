90787a4e5ceea6394a745ae57aa5bb2e341924e9
JonasDreessen <dreessenjonas95@gmail.com>
2019-08-14 12:17:50 +0200

    added translate npm

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
+#!/usr/bin/env node
+var readlineSync = require('readline-sync');
 var validator = require("email-validator");
-var stringToTranslate = "I Love Pizza";
 const axios = require('axios');
-var emailToValidate = "Jonas-Dreessen@outlook.com"
 
+// --------------------
+
+// Wait for user's response.
+var emailAdress = readlineSync.question('May I have your emailAdress? ');
+var emailToValidate = emailAdress;
 
 if(validator.validate(emailToValidate) === true){
     var correctEmailText = `${emailToValidate} is a valid format`;
     console.log(correctEmailText);
-    translateText();
+
 }
+var untranslatedText = readlineSync.question('Type the message you would like to translate (en --> it)');
+var stringToTranslate = untranslatedText;
 
 
 function translateText(){
-
+    console.log(stringToTranslate);
 axios.get(`https://api.mymemory.translated.net/get?q=${stringToTranslate}&langpair=en|it`)
   .then(function (response) {
+      //console.log(response);
     nodeOutput(response.data.responseData.translatedText);
   });
 }
+translateText();
 
   function nodeOutput(output){
       console.log(`${stringToTranslate} translated in English is ${output}`);
